9fe0c859efda271307c1f6339d4af7abb5f4f748
Priyanshi singh <95485651+Prinshi30@users.noreply.github.com>
2024-04-04 21:28:22 +0530

    Update Running Time and Complexity.java

--- Day 25: Running Time and Complexity/Running Time and Complexity.java
+++ Day 25: Running Time and Complexity/Running Time and Complexity.java
@@ -1,28 +1,29 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
-
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        final int N = sc.nextInt();
-        for (int i = 0; i < N; i++) {
-            if (isPrime(sc.nextInt()))
-                System.out.println("Prime");
-            else
-                System.out.println("Not prime");
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        while (T-- > 0) {
+            int n = scan.nextInt();
+            System.out.println(isPrime(n) ? "Prime" : "Not prime");
         }
+        scan.close();
     }
     
-    private static boolean isPrime(int num) {
-        if (num == 1) return false;
-        for (int i = 2; i < Math.sqrt(num); i++)
-            if (num % i == 0) return false;
+    private static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        } else if (n == 2) {     // account for even numbers now, so that we can do i+=2 in loop below
+            return true;
+        } else if (n % 2 == 0) { // account for even numbers now, so that we can do i+=2 in loop below
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 3; i <= sqrt; i += 2) { // skips even numbers for faster results
+            if (n % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
-
